c8222c6a656fdb8e0ff251d33863b63d1e43ae64
FooX-1999 <leao741fooda@gmail.com>
2025-01-18 23:18:31 -0300

    funcionamento do array e da limpeza do campo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomesAmigos = [];
+
+function adicionarAmigo(){
+    nomes = document.querySelector('input').value;
+    nomesAmigos.push(nomes);
+    console.log(nomesAmigos);
+    limparCampo();
+
+}  
+
+
+
+function limparCampo() {
+    let limpeza = document.querySelector('input');
+    limpeza.value = '';
+}
